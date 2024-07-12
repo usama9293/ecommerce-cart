e6fdde581d4db79db5314e32212c37b81395a65e
usama9293 <usamaarshad9293@gmail.com>
2024-07-12 11:54:52 +0500

    "cartitem completed"

--- src/Screens/Cart.jsx
+++ src/Screens/Cart.jsx
@@ -1,10 +1,25 @@
 import './Cart.css'
-
+import CartItem from "../Components/CartItem"
 import React from 'react'
 
 export default function Cart() {
   return (
-    <div>
+    <div className='cartscreen'>
+      <div className="left-side">
+        <h1>Shopping Cart</h1>
+        <CartItem/>
+      </div>
+      <div className="right-side">
+        <div className="cart-info">
+
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+
+        </div>
+        <div>
+          <button>Proceed to Checkout</button>
+        </div>
+      </div>
       
     </div>
   )
