e44ca29a21fdac70414bb71cfdee1fbcf7b8e2ab
usama9293 <usamaarshad9293@gmail.com>
2024-07-13 11:45:39 +0500

    "cart completed"

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import './CartItem.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './CartItem.css';
+import { Link } from 'react-router-dom';
 import { FaRegTrashAlt } from "react-icons/fa";
+
 const CartItem = () => {
-    return (
-        <div className='cartitems'>
-
-            <div className="cart-image">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg" alt="product name" />
-            </div>
-            <Link to={`/product/${111}`} className="cart-name">
-                <p>Product 1</p>
-            </Link>
-            <p className="cart-price">$499.99</p>
-            <select className="cart-select">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select>
-
-            <button className='del-item'>
-                <FaRegTrashAlt />
-            </button>
-        </div>
-    )
+  return (
+    <div className='cartitems'>
+      <div className="cart-image">
+        <img src="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg" alt="product name" />
+      </div>
+      <Link to={`/product/${111}`} className="cart-name">
+        <p>Product 1</p>
+      </Link>
+      <p className="cart-price">$499.99</p>
+      <select className="cart-select">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+      <button className='del-item'>
+        <FaRegTrashAlt />
+      </button>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
--- src/Screens/Cart.jsx
+++ src/Screens/Cart.jsx
@@ -1,26 +1,26 @@
-import './Cart.css'
-import CartItem from "../Components/CartItem"
-import React from 'react'
+import React from 'react';
+import './Cart.css';
+import CartItem from "../Components/CartItem";
 
-export default function Cart() {
+const Cart = () => {
   return (
     <div className='cartscreen'>
       <div className="left-side">
         <h1>Shopping Cart</h1>
-        <CartItem/>
+        <CartItem />
+        <CartItem /> {/* Add more CartItem components as needed */}
       </div>
       <div className="right-side">
         <div className="cart-info">
-
           <p>Subtotal (0) items</p>
           <p>$499.99</p>
-
         </div>
         <div>
           <button>Proceed to Checkout</button>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
+
+export default Cart;
